991bbef371b15bc0bea5d5d82210fccb63633611
vagrant <vagrant@localhost.localdomain>
2019-09-10 04:55:01 +0900

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+                <div>
+                  <textarea id="editor" name="name" rows="8" cols="40">
+        </textarea>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+        <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+        <script>
+            var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+        </script>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
